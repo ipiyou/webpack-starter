7ff61d52a1f817fefd4a0755a2b68472093efb66
ipiyou <tao710803@gmail.com>
2023-02-02 21:02:43 +0900

    라이센스 추가

--- webpack/end.js
+++ webpack/end.js
@@ -1,4 +1,9 @@
-// https://github.com/facebook/create-react-app/blob/main/packages/react-scripts/config/env.js
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 const REACT_APP = /^REACT_APP_/i;
 
 function getClientEnvironment(publicUrl) {
